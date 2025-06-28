dd69a30d8a0d4a966578b2b6f0d1e1bd5c94baa8
Sarahbatista99 <sarahlaylamellysa@gmail.com>
2025-06-28 19:57:11 -0300

    projeto conta banco

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,18 @@
-public class App {
+import java.util.Scanner;
+
+public class ContaTerminal{
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        var nome = scanner.next();
+        System.out.println("Olá, Qual o seu nome?");
+        var Agencia = scanner.nextInt();
+        System.out.println("Qual o número da sua agência ?");
+        var Conta = scanner.nextInt();
+        System.out.println("Qual o número da sua conta bancária? ");
+        var Saldo = scanner.nextFloat();
+        System.out.println("Qual seu saldo bancário?");
+       
+        System.out.println("Olá %S, o número da sua conta báncaria é %s e o número da sua agência é %s, o saldo bancário é de %s ?", nome, agencia, conta, saldo);
+
     }
 }
